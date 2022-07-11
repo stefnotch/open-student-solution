3e414dc2d9c7d0a4176a323f6657d9f421a2967a
stefnotch <brandmairstefan@gmail.com>
2022-07-11 10:12:58 +0200

    Opener

--- src/main.rs
+++ src/main.rs
@@ -27,41 +27,37 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let students = get_known_students(&algodat_abgaben_dir);
 
-    let args: Vec<String> = std::env::args().collect();
-    let mut mat_nr = if args.len() > 1 {
-        args[1].to_owned()
-    } else {
-        "".to_owned()
-    };
+    let program_args: Vec<String> = std::env::args().collect();
+    let mut mat_nr = program_args.get(1).unwrap_or(&"".to_string()).to_owned();
 
     // Get the matriculation number from the clipboard, if possible
     if !is_mat_nr(&mat_nr) {
-        match Clipboard::new().and_then(|mut clipboard| clipboard.get_text()) {
-            Ok(clipboard_text) => {
-                if is_mat_nr(&clipboard_text) {
-                    let message = format!("Select student: {}", clipboard_text);
-                    match inquire::Select::new(&message, vec![OkCancel::Ok, OkCancel::Cancel])
-                        .prompt_skippable()
-                    {
-                        Ok(Some(OkCancel::Ok)) => {
-                            mat_nr = clipboard_text;
-                        }
-                        _ => (),
-                    };
-                }
+        if let Ok(clipboard_text) = Clipboard::new().and_then(|mut clipboard| clipboard.get_text())
+        {
+            if is_mat_nr(&clipboard_text) {
+                let message = format!("Select student: {}", clipboard_text);
+                match inquire::Select::new(&message, vec![OkCancel::Ok, OkCancel::Cancel])
+                    .prompt_skippable()
+                {
+                    Ok(Some(OkCancel::Ok)) => {
+                        mat_nr = clipboard_text;
+                    }
+                    _ => (),
+                };
             }
-            Err(_) => {}
-        };
+        }
     }
 
     // Ask for a matriculation number
     let suggester = student_suggester(&students);
     while !is_mat_nr(&mat_nr) {
-        mat_nr = match inquire::Text::new("Matriculation number?")
+        match inquire::Text::new("Matriculation number?")
             .with_suggester(suggester.as_ref())
             .prompt()
         {
-            Ok(user_mat_nr) => user_mat_nr.trim().chars().take(8).collect(), // TODO: This is a biiit of a hack
+            Ok(user_mat_nr) => {
+                mat_nr = user_mat_nr.trim().chars().take(8).collect(); // TODO: This is a biiit of a hack
+            }
             Err(_) => {
                 println!("An error happened when asking for the mat.nr., try again later.");
                 return Ok(());
@@ -70,61 +66,70 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     // Don't use temp dir, since it's majorly cursed on Windows
-    let output_container_dir = algodat_dir.join("opened-solution");
-    fs::create_dir_all(&output_container_dir)?;
+    let student_output_dir = {
+        let output_container_dir = algodat_dir.join("opened-solution");
+        fs::create_dir_all(&output_container_dir)?;
 
-    let output_dir = output_container_dir.join("ad-".to_owned() + &mat_nr);
-    if output_dir.exists() && !output_dir.read_dir()?.next().is_none() {
-        println!("The {:?} directory already exists", &output_dir);
-        let options = vec![FileExistsOptions::Overwrite, FileExistsOptions::Cancel];
-        match inquire::Select::new("What do you want to do?", options).prompt() {
-            Ok(option) => match option {
-                FileExistsOptions::Overwrite => {
-                    fs::remove_dir_all(&output_dir)?;
-                }
-                FileExistsOptions::Cancel => {
-                    println!("Cancelled...quitting program");
-                    return Ok(());
-                }
-            },
-            Err(_) => {
-                println!(
+        let output_dir = output_container_dir.join("ad-".to_owned() + &mat_nr);
+        if output_dir.exists() && !output_dir.read_dir()?.next().is_none() {
+            println!("The {:?} directory already exists", &output_dir);
+            let options = vec![FileExistsOptions::Overwrite, FileExistsOptions::Cancel];
+            match inquire::Select::new("What do you want to do?", options).prompt() {
+                Ok(option) => match option {
+                    FileExistsOptions::Overwrite => {
+                        fs::remove_dir_all(&output_dir)?;
+                    }
+                    FileExistsOptions::Cancel => {
+                        println!("Cancelled...quitting program");
+                        return Ok(());
+                    }
+                },
+                Err(_) => {
+                    println!(
                     "An error happened while dealing with the existing directory, try again later."
                 );
-                return Ok(());
+                    return Ok(());
+                }
             }
         }
-    }
-
-    fs::create_dir_all(&output_dir)?;
+        output_dir
+    };
+    fs::create_dir_all(&student_output_dir)?;
+    println!("Created {:?}", &student_output_dir);
 
     // No chmod 0700, since we're on Windows and stuff
 
-    println!("Created {:?}", &output_dir);
-    let mut gitignore_file = File::create(output_dir.join(".gitignore"))?;
-    gitignore_file.write_all(b"/*")?; // Basically gitignore this entire folder
+    // Create gitignore
+    {
+        let mut gitignore_file = File::create(student_output_dir.join(".gitignore"))?;
+        gitignore_file.write_all(b"/*")?; // Basically gitignore this entire folder
+    }
 
-    // Copy the Frameworks
+    // Copy the frameworks, like /ag2/frameworks/P4
     let framework_dirs = get_framework_dirs(algodat_dir)?;
-    let framework_files: Vec<_> = framework_dirs.iter().map(fs::DirEntry::file_name).collect();
+    let framework_names: Vec<_> = framework_dirs.iter().map(fs::DirEntry::file_name).collect();
     {
         let options = fs_extra::dir::CopyOptions::new();
         let from_paths: Vec<_> = framework_dirs.iter().map(fs::DirEntry::path).collect();
-        copy_items(&from_paths, &output_dir, &options)?;
+        copy_items(&from_paths, &student_output_dir, &options)?;
     }
+
+    let mut open_options: Vec<OpenOption> = Vec::new();
 
     // Copy the abgabe PDF
     if let Some(student_pdf_file) =
         find_student_file(algodat_abgaben_dir.join("Berichte"), &mat_nr)?
     {
-        fs::copy(
-            &student_pdf_file,
-            output_dir.join(
-                student_pdf_file
-                    .file_name()
-                    .unwrap_or(&std::ffi::OsString::from("abgabe.pdf")),
-            ),
-        )?;
+        let student_pdf_file_result = student_output_dir.join(
+            student_pdf_file
+                .file_name()
+                .unwrap_or(&std::ffi::OsString::from("abgabe.pdf")),
+        );
+        fs::copy(&student_pdf_file, &student_pdf_file_result)?;
+        open_options.push(OpenOption::PDF(
+            "Student PDF".into(),
+            student_pdf_file_result,
+        ));
     } else {
         println!("Couldn't find the abgabe PDF for {:?}", mat_nr);
     }
@@ -133,10 +138,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     {
         for entry in fs::read_dir(algodat_abgaben_dir)? {
             let source_dir = entry?;
-            if !framework_files.contains(&source_dir.file_name()) {
+            if !framework_names.contains(&source_dir.file_name()) {
                 continue;
             }
-            let result_dir = output_dir.join(source_dir.file_name());
+            let result_dir = student_output_dir.join(source_dir.file_name());
 
             if let Some(student_code_file) = find_student_file(source_dir.path(), &mat_nr)? {
                 println!(
@@ -151,7 +156,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
                 let mut named_student_code_file = source_dir.file_name();
                 named_student_code_file.push("-StudentSolutionImplementation.java");
-                fs::copy(&student_code_file, output_dir.join(named_student_code_file))?;
+                fs::copy(
+                    &student_code_file,
+                    student_output_dir.join(named_student_code_file),
+                )?;
+
+                open_options.push(OpenOption::Code(
+                    source_dir.file_name().to_string_lossy().into(),
+                    result_dir,
+                ));
             } else {
                 println!(
                     "Couldn't find the code file for {:?} {:?}",
@@ -165,7 +178,27 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
 
-    open::that(&output_dir)?;
+    open::that(&student_output_dir)?;
+
+    loop {
+        match inquire::Select::new("Open", open_options.clone()).prompt_skippable() {
+            Ok(Some(OpenOption::PDF(_, path))) => {
+                open::that(&path)?;
+            }
+            Ok(Some(OpenOption::Code(_, path))) => {
+                open::with(path, &my_config.intellij)?;
+                continue;
+            }
+            Ok(None) => {
+                println!("Quitting program");
+                return Ok(());
+            }
+            Err(e) => {
+                println!("An error happened: {:?}", e);
+                return Ok(());
+            }
+        }
+    }
     // TODO: Show "open PDF" and "open P1/P2/P3" buttons
     // TODO: Protip, use https://plugins.jetbrains.com/plugin/14494-pdf-viewer and then you only have to share your screen
     // TODO: Open PDF with default program (or browser) and P1/P2/P3 with explorer
@@ -173,8 +206,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // TODO: Ask "open with Intellij/default program"
 
     // TODO: intellij (uh oh) with https://github.com/oliverschwendener/ueli/blob/dev/src/main/executors/application-searcher.ts or https://github.com/microsoft/windows-rs with https://stackoverflow.com/questions/908850/get-installed-applications-in-a-system
-
-    Ok(())
 }
 
 fn find_student_file<P>(path: P, mat_nr: &str) -> std::io::Result<Option<PathBuf>>
@@ -203,12 +234,14 @@ fn get_framework_dirs(algodat_dir: &Path) -> std::io::Result<Vec<fs::DirEntry>> 
 #[derive(Serialize, Deserialize)]
 struct MyConfig {
     algodat_dir: String,
+    intellij: String,
 }
 
 impl Default for MyConfig {
     fn default() -> Self {
         Self {
             algodat_dir: ".".into(),
+            intellij: "".into(),
         }
     }
 }
@@ -239,6 +272,21 @@ impl fmt::Display for FileExistsOptions {
         match self {
             FileExistsOptions::Overwrite => write!(f, "Overwrite"),
             FileExistsOptions::Cancel => write!(f, "Cancel"),
+        }
+    }
+}
+
+#[derive(Clone)]
+enum OpenOption {
+    PDF(String, PathBuf),
+    Code(String, PathBuf),
+}
+
+impl fmt::Display for OpenOption {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            OpenOption::PDF(name, _) => write!(f, "{}", name),
+            OpenOption::Code(name, _) => write!(f, "{} code", name),
         }
     }
 }
